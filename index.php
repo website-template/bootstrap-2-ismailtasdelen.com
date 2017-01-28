ef4bb12a86b5d7fd4f7564777cf1bc0348abb6cf
İSMAİL TAŞDELEN <pentestdatabase@gmail.com>
2017-01-28 17:07:24 +0200

    master

---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport"    content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author"      content="İsmail (https://github.com/ismailtasdelen)">
+	
+	<title>İsmail Taşdelen | Web Page</title>
+
+	<link rel="shortcut icon" href="assets/images/website_ico.png">
+	
+	<!-- Bootstrap itself -->
+	<link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+	<!-- Custom styles -->
+	<link rel="stylesheet" href="assets/css/desing.css">
+
+	<!-- Fonts -->
+	<link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	<link href='http://fonts.googleapis.com/css?family=Wire+One' rel='stylesheet' type='text/css'>
+</head>
+
+<!-- use "theme-invert" class on bright backgrounds, also try "text-shadows" class -->
+<body class="theme-invert">
+
+<nav class="mainmenu">
+	<div class="container">
+		<div class="dropdown">
+			<button type="button" class="navbar-toggle" data-toggle="dropdown"><span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+			<!-- <a data-toggle="dropdown" href="#">Dropdown trigger</a> -->
+			<ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
+				<li><a href="#head" class="active">Home</a></li>
+				<li><a href="#about">About Me</a></li>
+				<li><a href="#contact">Contact</a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
+
+
+<!-- Main (Home) section -->
+<section class="section" id="head">
+	<div class="container">
+
+		<div class="row">
+			<div class="col-md-10 col-lg-10 col-md-offset-1 col-lg-offset-1 text-center">	
+
+				<!-- Site Title, your name, HELLO msg, etc. -->
+				<h1 class="title">Ismail Tasdelen</h1>
+				<h2 class="subtitle">Network Securtiy &amp; Penetration Tester</h2>
+
+				<!-- Short introductory (optional) -->
+				<h3 class="tagline">
+					
+				</h3>
+	
+			</div> <!-- /col -->
+		</div> <!-- /row -->
+	
+	</div>
+</section>
+
+<!-- Second (About) section -->
+<section class="section" id="about">
+	<div class="container">
+	
+		<h2 class="text-center title">About me</h2>
+		<div class="row">
+		<h3 class="tagline"><center>
+		I have done penetration testing and vulnerability screening tests on many banks, private sector and public institutions. I have reported the deficits I have found and made the deficits closed. Apart from that, I am also professionally involved in software. If you communicate, you may be sure to return as soon as possible.
+		</center></h3>
+		</div>
+	</div>
+</section>
+
+</section>
+<!-- Fourth (Contact) section -->
+<section class="section" id="contact">
+	<div class="container">
+	
+		<h2 class="text-center title">Get in touch</h2>
+
+		<div class="row">
+			<div class="col-sm-8 col-sm-offset-2 text-center">
+				<p><br>If you communicate, you can be confident that you will be back as soon as possible.</br></p>
+				<p><b>ismailtasdelen@std.sehir.edu.tr<br></p>
+				<ul class="list-inline list-social">
+					<li><a href="https://twitter.com/@RTiwit" class="btn btn-link"><i class="fa fa-twitter fa-fw"></i> Twitter </a></li>
+					<li><a href="https://github.com/ismailtasdelen" class="btn btn-link"><i class="fa fa-github fa-fw"></i> GitHub </a></li>
+					<li><a href="https://www.linkedin.com/in/ismailtasdelen" class="btn btn-link"><i class="fa fa-linkedin fa-fw"></i> LinkedIn </a></li>
+				</ul>
+			</div>
+		</div>
+
+	</div>
+</section>
+
+
+<!-- Load js libs only when the page is loaded. -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+<script src="assets/js/moderndesing.custom.js"></script>
+<!-- Custom template scripts -->
+<script src="assets/js/desing.js"></script>
+</body>
+</html>
